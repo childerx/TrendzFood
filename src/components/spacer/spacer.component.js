16bff645f96be0ced1fff3ee643f7d028c7cb22d
Nana Kwame <iabu007@st.ug.edu.gh>
2023-08-07 23:34:05 +0000

    Update spacer.component.js

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -14,7 +14,7 @@ const positionVariant = {
   bottom: "marginBottom",
 };
 
-const getVariant = (pos, size, theme) => {
+const getVariant = (posi, size, theme) => {
   const sizeIndex = sizesVariant[size];
   const property = positionVariant[position];
   const value = theme.space[sizeIndex];
